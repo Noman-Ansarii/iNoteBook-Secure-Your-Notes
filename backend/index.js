cba92da4862f756b65fb3580e35d6a68005857c2
Noman Ansariii. <ansarigee300@gmail.com>
2024-05-13 11:15:58 +0500

    Cors

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,23 @@
 require('dotenv').config({ path: './.env' });
 
+const cors = require('cors');
 const express = require('express')
-var cors = require('cors');
 const connectToMongo = require('./db.js');
+
+const corsConfig = {
+  origin: '*',
+  credentials: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+}
+
+app.use(cors(corsConfig));
 
 connectToMongo();
 const app = express()
 const PORT = 8000;
 
-app.use(cors());
 
 app.use(express.json())
 
